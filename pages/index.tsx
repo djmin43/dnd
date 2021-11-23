49b6e38352d2009d6c41cf9b4f42049ea508288f
jay <dj.min43@gmail.com>
2021-11-23 14:34:06 +0900

    reset

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,9 @@
 import type { NextPage } from 'next'
-import initialData from '../src/initial-data'
 
 const Home: NextPage = () => {
   return (
-    <div>
-
-    </div>
+  <div className="App">
+  </div>
   )
 }
 
