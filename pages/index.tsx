c2e4fbe111d2f494aa4363e6c0de275ca9fe9364
jay <dj.min43@gmail.com>
2021-11-23 15:04:29 +0900

    types for beautiful dnd

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,10 @@
 import type { NextPage } from 'next'
+import MainPart from '../src/components/MainPart'
 
 const Home: NextPage = () => {
   return (
   <div className="App">
+    <MainPart />
   </div>
   )
 }
---
+++ src/components/MainPart.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { DragDropContext } from 'react-beautiful-dnd'
+
+const MainPart = () => {
+  return (
+    <div>
+      h
+    </div>
+  )
+}
+
+export default MainPart
