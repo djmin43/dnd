a3f3a523c6f381d2568130bcde931a43ce2b9074
jay <dj.min43@gmail.com>
2021-11-24 15:49:57 +0900

    add droppables

--- src/components/KanbanBoard.tsx
+++ src/components/KanbanBoard.tsx
@@ -2,13 +2,22 @@ import React, { useEffect, useState } from 'react'
 import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd'
 import { v4 as uuidv4 } from 'uuid'
 
+const itemsFromBackend = [
+  {id: uuidv4(), content: 'First task'},
+  {id: uuidv4(), content: 'Second task'},
+]
+
 const columnsFromBackend = [
   {
-
+    [uuidv4()]: {
+      name: 'Todo',
+      items: [itemsFromBackend]
+    }
   }
 ]
 
 const KanbanBoard = () => {
+  const [columns, setColumns] = useState(columnsFromBackend)
 
   const [winReady, setWinReady] = useState(false)
 
@@ -21,7 +30,26 @@ const KanbanBoard = () => {
 
       {winReady &&
         <DragDropContext onDragEnd={result => console.log(result)}>
-          
+          {Object.entries(columns).map(([id, column]) => {
+            <Droppable droppableId={id}>
+              {(provided, snapshot) => {
+                return (
+                  <div
+                    {...provided.droppableProps}
+                    ref={provided.innerRef}
+                    style={{
+                      background: snapshot.isDraggingOver ? 'lightblue' : 'lightgrey',
+                      padding: 4,
+                      width: 250,
+                      minHeight: 500
+                    }}
+                  >
+                    
+                  </div>
+                )
+              }}
+            </Droppable>
+          })}
         </DragDropContext> 
       }
     </div>
