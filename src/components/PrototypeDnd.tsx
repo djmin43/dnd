663bcec0ad7e36e1052de802b9f1cd897810c432
jay <dj.min43@gmail.com>
2021-11-26 15:19:40 +0900

    set types

--- src/components/PrototypeDnd.tsx
+++ src/components/PrototypeDnd.tsx
@@ -1,14 +1,98 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd'
 import { css } from '@emotion/css'
-import { feDevs, DevList, Developer } from '../prototype-list'
+import { feDevs, beDevs, DevList, Developer } from '../prototype-list'
 
 const PrototypeDnd = () => {
+
+  const [isListActive, setIsListActive] = useState(false)
+
+  const [items, setItems] = useState(feDevs)
+
+  useEffect(() => {
+    setIsListActive(true)
+  }, [])
+
+  const onDragEnd = (result: any) => {
+    if (!result.destination) {
+      return;
+  }
+    console.log(result)
+    const newItems = [...items]
+    const [removed] = newItems.splice(result.source.index, 1)
+    newItems.splice(result.destination.index, 0, removed)
+    setItems(newItems)
+  }
+
   return (
     <div>
-      
+      FE List
+      {isListActive &&
+        <DragDropContext onDragEnd={onDragEnd}>
+          <Droppable droppableId="fe" direction="vertical" type="front-end">
+            {(provided, snapshot) => (
+              <div
+                className={boardStyle}
+                {...provided.droppableProps}
+                ref={provided.innerRef}
+                
+              >
+                {feDevs.map((card: Developer, index: number) => (
+                  <Draggable draggableId={card.id} index={index} key={card.id}>
+                    {(provided, snapshot) => (
+                      <div
+                        className={itemStyle}
+                        ref={provided.innerRef}
+                        {...provided.draggableProps}
+                        {...provided.dragHandleProps}
+                      >
+                        {card.name}
+                      </div>
+                    )}
+                  </Draggable>
+                ))}
+              </div>
+            ) }
+          </Droppable>
+          <Droppable droppableId="fe2" direction="vertical" type="back-end" >
+            {(provided, snapshot) => (
+              <div
+                className={boardStyle}
+                {...provided.droppableProps}
+                ref={provided.innerRef}
+                
+              >
+                {beDevs.map((card: Developer, index: number) => (
+                  <Draggable draggableId={card.id} index={index} key={card.id}>
+                    {(provided, snapshot) => (
+                      <div
+                        className={itemStyle}
+                        ref={provided.innerRef}
+                        {...provided.draggableProps}
+                        {...provided.dragHandleProps}
+                      >
+                        {card.name}
+                      </div>
+                    )}
+                  </Draggable>
+                ))}
+              </div>
+            ) }
+          </Droppable>
+        </DragDropContext>
+      }
     </div>
   )
 }
+
+const boardStyle = css`
+  width: 30vw;
+`
+
+const itemStyle = css`
+  border: 1px solid black;
+  background-color: pink;
+  border-radius: 4px;
+`
 
 export default PrototypeDnd
--- src/prototype-list.ts
+++ src/prototype-list.ts
@@ -6,15 +6,21 @@ export interface Developer {
 export type DevList = Developer[]
 
 export const feDevs = [
-{id: '1', name: 'Chiho Yoon'},
-{id: '2', name: 'John Kim'},
-{id: '3', name: 'Sally Lee'},
-{id: '4', name: 'David Park'},
-{id: '5', name: 'James Min'},
-{id: '6', name: 'Anna An'},
-{id: '7', name: 'Ethan Kim'},
+  {id: '1', name: 'Chiho Yoon'},
+  {id: '2', name: 'John Kim'},
+  {id: '3', name: 'Sally Lee'},
+  {id: '4', name: 'David Park'},
+  {id: '5', name: 'James Min'},
+  {id: '6', name: 'Anna An'},
+  {id: '7', name: 'Ethan Kim'},
 ]
 
 export const beDevs = [
-
+  {id: '8', name: 'Francis Kim'},
+  {id: '9', name: 'Ted Yoo'},
+  {id: '10', name: 'Shavkat'},
+  {id: '11', name: 'Jaehyoung Lee'},
+  {id: '12', name: 'Jack Min'},
+  {id: '13', name: 'Paul'},
+  {id: '14', name: 'YNC'},
 ]
